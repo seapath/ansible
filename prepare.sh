580b658cb713c67ba33eceba15a1b28bbf6bb5d6
Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
2020-11-17 12:06:44 +0100

    prepare.sh: check ceph-ansible dependencies
    ceph-ansible requires the python module "netaddr" and is only compatible
    with the 2.9 version of Ansible.
    
    Verify this requirement in prepare.sh script and update the
    documentation.
    
    Signed-off-by: Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>

--- prepare.sh
+++ prepare.sh
@@ -16,9 +16,25 @@ if ! command -v ansible &>/dev/null ; then
     exit 1
 fi
 
+echo "Test Ansible version is 2.9"
+if ! ansible --version | grep -q 'ansible 2.9' ; then
+    echo "Error: Installed version of ansible must match 2.9" 1>&2
+    echo "See README.adoc for instructions" 1>&2
+fi
+
 echo "Test ansible-galaxy is installed"
 if ! command -v ansible-galaxy &>/dev/null ; then
     echo "Error: ansible-galaxy must be installed to continue" 1>&2
+    echo "See README.adoc for instructions" 1>&2
+    exit 1
+fi
+
+echo "Test python3 netaddr module is installed"
+if ! cat << EOF | python3 &>/dev/null
+import netaddr
+EOF
+then
+    echo "Error: python3 netaddr module must be installed" 1>&2
     echo "See README.adoc for instructions" 1>&2
     exit 1
 fi
