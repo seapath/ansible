c68688c910f4626949cfaf4fa72166e54b7bc010
Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
2020-11-23 18:05:36 +0100

    network_playbook.yaml: change network configuration
    Updates how to configure the network to track specification changes.
    
    The configuration of the public network is done in the same way for all
    hosts. This is done by defining the following variables:
        - network_interface: The network interface to use as public network.
          It can be a physical network interface or an OVS bridge.
        - dns_server: the DNS server address
        - ip_addr: the IP address
        - gateway_addr: the gateway IP address
        - subnetwork_suffix: the optional subnetwork suffix (default is 24)
    
    Other network interfaces can be configured by using the list variable
    custom_network as described in roles/systemd_networkd/README.md.
    
    Open vSwitch bridges are defined with the ovs_bridges list variable.
    Each bridge can work with or without DPDK.
    
    Signed-off-by: Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>

--- templates/setup_ovs.sh
+++ templates/setup_ovs.sh
@@ -5,18 +5,15 @@
 
 set -e
 
-modprobe "{{ dpdk_module }}"
-dpdk-devbind --force  --bind="{{ dpdk_module }}" "{{ dpdk_nic }}"
-if ip addr |grep -q "ovsbr:" ; then
-    ovs-vsctl del-br ovsbr
+dpdk_module="{{ dpdk_module | default() }}"
+
+if [ -n "${dpdk_module}" ] ; then
+    modprobe "${dpdk_module}"
 fi
-ovs-vsctl add-br ovsbr -- set bridge ovsbr datapath_type=netdev
-ovs-vsctl add-port ovsbr dpdk-p0 -- set Interface dpdk-p0 type=dpdk \
-    "options:dpdk-devargs={{ dpdk_nic }}"
-    for i in $(seq 0 $(({{ ovs_number_of_virtual_ports }} -1))) ; do
-    ovs-vsctl add-port ovsbr dpdkvhostuser$i -- set Interface dpdkvhostuser$i \
-        type=dpdkvhostuserclient
-    ovs-vsctl set Interface dpdkvhostuser$i \
-        options:vhost-server-path="/var/run/openvswitch/dpdkvhostuser$i"
-done
-chmod 777 /var/run/openvswitch
+
+if [ -d /opt/setup/setup_ovs.d ] ; then
+    run-parts /opt/setup/setup_ovs.d
+fi
+
+chown :qemu /var/run/openvswitch
+chmod 775 /var/run/openvswitch
