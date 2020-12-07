9590ca2984ea1c416ea8461a72a7b3ee2609020e
Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
2020-12-07 18:55:07 +0100

    tests: add tests for disks roles
    Add test playbook to test the roles managing disks.
    Add documentation associated with these tests.
    
    Signed-off-by: Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>

---
+++ tests/generate_test_data.sh
@@ -0,0 +1,14 @@
+# Copyright (C) 2020, RTE (http://www.rte-france.com)
+# SPDX-License-Identifier: Apache-2.0
+
+# The script will generate all test data in testdata directory
+if [ $# -ne 0 ] ; then
+    echo "The script doesn't take any parameters"
+    exit 1
+fi
+
+cd "$(dirname "$(readlink -f \\"$0\\")")"
+rm -rf testdata
+mkdir testdata
+qemu-img create -f qcow2 testdata/disk.qcow2 5M
+fallocate -l 5M testdata/disk
