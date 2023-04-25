5f18f17721a8cf54b401208207226223271dca13
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2023-04-25 13:18:49 +0200

    snmp scripts: fix path
    All scripts run with sudo should only use absolute path, as it avoid a
    user to run an arbitrary script by renaming it.
    
    Signed-off-by: Erwann Roussy <erwann.roussy@savoirfairelinux.com>

--- src/debian/virt-df.sh
+++ src/debian/virt-df.sh
@@ -6,16 +6,16 @@ done
 for guest in `virsh --connect qemu:///system list --all | awk 'NR>2 { print $2 }' | sed /^$/d`
 do
   mkdir -p /t
-  umount /t 2>/dev/null
+  /usr/bin/umount /t 2>/dev/null
   devname=`rbd map --read-only system_"$guest"`
   for part in $devname"p"*
   do
-    mount -o ro,noload $part /t 2>/dev/null
+    /usr/bin/mount -o ro,noload $part /t 2>/dev/null
     returncode=$?
     if [ $returncode -eq 0 ]; then
   	  df -k | grep $part | awk -v guest="$guest" '{ print guest ":" $1 " total:" $2 " used:" $3 " available:" $4 " disk-usage:" $5 }'
     fi
-    umount /t 2>/dev/null
+    /usr/bin/umount /t 2>/dev/null
   done
   rbd unmap $devname
 done
