dc52a9aae555716e6947f2730d616887163ab351
Thibault PEYPELUT <thibault.peypelut@smile.fr>
2023-07-06 18:39:57 +0200

    Imports all variables of ceph-group_vars in the inventory
    When we define parameters in the ceph-group_vars, we need run a script (prepare).
    So, to avoid errors or lost time to debug this, it's better to reduce it.
    
    Signed-off-by: Thibault PEYPELUT <thibault.peypelut@smile.fr>

--- prepare.sh
+++ prepare.sh
@@ -57,9 +57,6 @@ git submodule update --init --force
 echo "Copy ceph-ansible site.yml"
 cp -vf src/ceph-ansible-site.yaml ceph-ansible/site.yml
 
-echo "Copy ceph-group_vars"
-cp -vf vars/ceph_group_vars/*.yml ceph-ansible/group_vars/
-
 echo "Patch ceph-ansible"
 find src/ceph-ansible-patches -type f -name "*.diff" -exec git -C ceph-ansible \
     apply ../{} \;
