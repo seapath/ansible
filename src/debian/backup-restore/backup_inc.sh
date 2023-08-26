773872552a021c07a4a3c60eb5cfc77eae79d9dd
Florent CARLI <florent.carli@rte-france.com>
2023-08-26 15:14:51 +0200

    backup-restore: remote shell as config item + restore metadatas
    backup-restore uses rsync to connect to the offsite storage server, with the -e (remote shell) option.
    This commit sets the default rsync remote shell to "ssh", but it can now be overriden in the /etc/backup-restore.conf file
    This it particularly useful to change the port used by ssh.
    
    Also, this commit makes the restore feature also restore the metadata of the guest (as saved during backup). This will make the livemigration_user setting useless (livemigration_user will be part of the restored metadata).
    
    Signed-off-by: Florent CARLI <florent.carli@rte-france.com>

--- src/debian/backup-restore/backup_inc.sh
+++ src/debian/backup-restore/backup_inc.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 local_dir=$1
-remote_dir=$2
+remote_shell=$2
+remote_dir=$3
 
 [ -z "$local_dir" ] && { echo "var local_dir empty"; exit 1; }
-[ -z "$remote_dir" ] && { echo "var remote_dir empty"; exit 2; }
+[ -z "$remote_shell" ] && { echo "var remote_shell empty"; exit 2; }
+[ -z "$remote_dir" ] && { echo "var remote_dir empty"; exit 3; }
 
 #local_dir="/data2/backup_ceph_"
+#remote_shell="ssh -p 22"
 #remote_dir="cephbackup@ip:/mnt/nasopf/seapath/backups_ceph_cluster_proj/"
 
 d=`date +%Y%m%d%H%M`
@@ -23,7 +26,14 @@ do
   rbd export-diff --from-snap $latest rbd/$i@$d $latest_full/"$i"_"$latest"_"$d".diff
   echo backuping vm xml
   rbd image-meta get rbd/$i xml > $latest_full/$i-$d.xml
-  echo backuping metadata
-  rbd image-meta list rbd/$i > $latest_full/$i-meta-$d.txt
+  echo backuping metadata all
+  rbd image-meta list rbd/$i > $latest_full/$i-metaall-$d.txt
+  echo backuping metadata one by one
+  guest=$(echo $i | sed s/^system_//)
+  for j in `python3 /usr/local/bin/get_metadata.py $guest`
+  do
+    echo "   " $j
+    rbd image-meta get rbd/$i $j > $latest_full/$i-meta-$j-$d.txt
+  done
 done
-rsync -ave ssh --progress $latest_full $remote_dir
+rsync -ave "$remote_shell" --progress $latest_full $remote_dir
---
+++ src/debian/backup-restore/restore_vm.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+local_tmp_dir=$1
+remote_shell=$2
+remote_dir=$3
+fulldatedir=$4
+guest=$5
+incdate=$6
+
+[ -z "$local_tmp_dir" ] && { echo "var local_tmp_dir empty"; exit 1; }
+[ -z "$remote_shell" ] && { echo "var remote_shell empty"; exit 2; }
+[ -z "$remote_dir" ] && { echo "var remote_dir empty"; exit 3; }
+[ -z "$fulldatedir" ] && { echo "var fulldatedir empty"; exit 4; }
+[ -z "$guest" ] && { echo "var guest empty"; exit 5; }
+[ -z "$incdate" ] && { echo "var incdate empty"; exit 6; }
+
+f="$local_tmp_dir""$d"
+mkdir -p $f
+
+#called with : ./restore_vm.sh /data2/tmp "ssh -p 22" cephbackup@ip:/mnt/nasopf/seapath/backups_ceph_cluster_lot1/ backup_ceph_202203110733 VM1 202203110836
+#local_tmp_dir=/data2/tmp
+#remote_dir=cephbackup@ip:/mnt/nasopf/seapath/backups_ceph_cluster/
+#fulldatedir=backup_ceph_202203110733
+#guest=VM2
+#incdate=202203110836
+
+echo $local_tmp_dir
+echo $remote_shell
+echo $remote_dir
+echo $fulldatedir
+echo $guest
+echo $incdate
+
+fulldate=`echo $fulldatedir | sed -e "s/backup_ceph_//"`
+
+echo Removing tmp local dir
+echo rm -rf "$local_tmp_dir"/*, press enter to proceed
+read
+rm -rf "$local_tmp_dir"/*
+echo
+echo copying files locally
+echo rsync -ave "$remote_shell" --progress $remote_dir$fulldatedir/*_"$guest"-* $local_tmp_dir/
+rsync -ave "$remote_shell" --progress $remote_dir$fulldatedir/*_"$guest"-* $local_tmp_dir/
+echo rsync -ave "$remote_shell" --progress $remote_dir$fulldatedir/*_"$guest"_* $local_tmp_dir/
+rsync -ave "$remote_shell" --progress $remote_dir$fulldatedir/*_"$guest"_* $local_tmp_dir/
+echo
+echo creating vm xml with no disk
+echo python3 /usr/local/bin/remove_disk_xml.py $local_tmp_dir/system_"$guest"-"$incdate".xml $local_tmp_dir/system_"$guest"-"$incdate"-nodisk.xml
+python3 /usr/local/bin/remove_disk_xml.py $local_tmp_dir/system_"$guest"-"$incdate".xml $local_tmp_dir/system_"$guest"-"$incdate"-nodisk.xml
+echo
+echo creating base vm
+echo vm-mgr create -n $guest --force --disable --xml $local_tmp_dir/system_"$guest"-"$incdate"-nodisk.xml -i $local_tmp_dir/system_"$guest"_"$fulldate".qcow2
+vm-mgr create -n $guest --force --disable --xml $local_tmp_dir/system_"$guest"-"$incdate"-nodisk.xml -i $local_tmp_dir/system_"$guest"_"$fulldate".qcow2
+echo
+echo creating base snapshot
+echo rbd snap create system_"$guest"@$fulldate
+rbd snap create system_"$guest"@$fulldate
+echo
+for difffile in `ls $local_tmp_dir/*.diff`
+do
+  datediff=`echo $difffile | sed -e s/.*_// -e s/\.diff//`
+  if [ $datediff -le $incdate ]; then
+    echo $difffile
+    echo rbd import-diff $difffile rbd/system_"$guest"
+    rbd import-diff $difffile rbd/system_"$guest"
+  fi
+done
+echo
+echo Restoring metadata
+for metafile in $(ls $local_tmp_dir/system_${guest}-meta-*-${incdate}.txt)
+do
+  key=$(echo $metafile | sed -e s/.*system_${guest}-meta-// -e s/-${incdate}.txt//)
+  echo "metadata name = " $key
+  echo rbd image-meta set system_${guest} $key "$(cat $metafile)"
+  rbd image-meta set system_${guest} $key "$(cat $metafile)"
+done
+echo
+echo Starting VM
+echo vm-mgr enable -n $guest
+vm-mgr enable -n $guest
+exit
