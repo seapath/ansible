291a1570d401dfeb39d37b7018668d690d2354f1
Florent Carli <florent.carli@rte-france.com>
2024-01-31 15:41:53 +0100

    snmp: better data curating in the smartctl exposition
    We used to expose the whole "smartctl -a" output with snmp but this poses 2 problems:
    - the disks were refered to with the standard (and non deterministic) sda, sdb naming scheme
    - the smartctl -a output is often too big for a single snmp message
    
    This commits reduces the data to information about the disk, result of self-test and attributes listing, but also add the /dev/disk/by-path information to be certain of what disk the data refers to.
    
    Signed-off-by: Florent Carli <florent.carli@rte-france.com>

--- src/debian/snmp_smartctl.sh
+++ src/debian/snmp_smartctl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 /usr/sbin/smartctl --scan | grep -E "^/dev/(sd|nvme)" | awk '{ print $1 }' | while read i; do
-  echo ------------------------------------------
   echo $i
-  echo ------------------------------------------
-  /usr/sbin/smartctl -a $i
+  j=$(echo $i | sed "s/\/dev\///")
+  k=$(/usr/bin/udevadm info -q symlink --path=/sys/block/$j | tr " " "\n" | sort | grep 'disk/by-path' | head -n 1 | awk '{print "/dev/" $1}')
+  echo $k
+  smartctl --attributes -H $i | sed '0,/^=== START OF READ SMART DATA SECTION ===$/d'
   echo ------------------------------------------
 done
