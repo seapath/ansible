7ececce9904510379023e2afee0fe0e64cfc0218
Florent CARLI <florent.carli@rte-france.com>
2024-07-12 13:42:02 +0200

    cleanup: remove obsolete files
    - exemples folder: all this is dealt with by vm_manager and the deploy_vm playbooks.
    - vm_templates: they are now in /templates/vm
    - module_documentation: the ansible module documentation is published on ansible-galaxy (seapath.cluster_vm)
    - roles: seapath only supports one-disk vm + all the rest is dealt with by vm_manager and the deploy_vm playbooks
    - tests/tests_results: all this is dealt with by cukinia / cukinia-tests and the CI
    - vm_images: this was for an example, the user can create the folder if he needs
    
    Signed-off-by: Florent CARLI <florent.carli@rte-france.com>
    Signed-off-by: Erwann Roussy <erwann.roussy@savoirfairelinux.com>

--- generate_module_documentation.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# Copyright (C) 2021, RTE (http://www.rte-france.com)
-# SPDX-License-Identifier: Apache-2.0
-
-
-set -e
-cd module_documentation_sources
-python3 plugin_formatter.py -M ../library -T templates/ -o source -A 2.9 -P module
-sphinx-build source ../module_documentation
