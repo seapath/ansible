c5bea136aee322cc38464f6de1971a877fabc12d
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2024-08-23 10:44:04 +0200

    roles/yocto/kerel_params: add role
    Transform the playbook cluster_setup_configure_hosts.yaml in a role.
    
    Signed-off-by: Erwann Roussy <erwann.roussy@savoirfairelinux.com>

--- roles/yocto/kernel_params/files/find_boot_partition.sh
+++ roles/yocto/kernel_params/files/find_boot_partition.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Copyright (C) 2024 Savoir-faire Linux, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+# This script detect the current rootfs partition and echo the associated efi
+# partition. It is used to mount the active efi partition.
 
 rootfs_part=$(mount | awk '/\/ / { print $1 }')
 disk_name="${rootfs_part: : -1}"
