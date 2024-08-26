ade2dbd6b0cd948d77e5f1fd8b4d07ec07eff50b
Florent CARLI <florent.carli@rte-france.com>
2024-08-26 20:59:30 +0200

    move vmmgrapi code to role
    Signed-off-by: Florent CARLI <florent.carli@rte-france.com>

---
+++ roles/vmmgrapi/files/wsgi.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2024, RTE (http://www.rte-france.com)
+# SPDX-License-Identifier: Apache-2.0
+
+from vm_manager.vm_manager_api import app
+
+if __name__ == "__main__":
+    app.run()
--- src/debian/vmmgrapi/wsgi.py
+++
@@ -1,4 +0,0 @@
-from vm_manager.vm_manager_api import app
-
-if __name__ == "__main__":
-    app.run()
